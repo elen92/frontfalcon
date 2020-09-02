ee3b27fe9407055ae66e14652e21ff540cf11094
Juan <jennifer.cortes.gu@gmail.com>
2020-09-02 18:10:39 -0500

    Se crea el formulario de contacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { NombreComponenteComponent } from './Components/nombre-componente/nombre-componente.component';
+import { ContactregisterComponent } from './Components/contactregister/contactregister.component';
+import { ContactRegisterComponent } from './components/contact-register/contact-register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    NombreComponenteComponent,
+    ContactregisterComponent,
+    ContactRegisterComponent
   ],
   imports: [
     BrowserModule
