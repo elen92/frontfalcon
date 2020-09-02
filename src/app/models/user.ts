a78bb31623c5dd1ad47d30bb30b1ff4e5e6a74dc
diegomez-ang <diego.gomezfswd@gmail.com>
2020-09-02 18:53:54 -0500

    modelo usuario

---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export interface User{
+    firstName: String;
+    lastName: String;
+    email: String;
+    phone: Number;
+    password: String;
+    photo: String;
+    nationality: String;
+    address: String;
+    idDocument: String
+} 
