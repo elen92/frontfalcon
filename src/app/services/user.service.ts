cab0543ba7f453163b6c39997a4def7faba33b98
dallaswolf1896 <roma900ac@gmail.com>
2020-09-03 18:58:22 -0500

    bugs fixed on USer Register

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,9 +6,14 @@ import { User } from '../models/user';
   providedIn: 'root'
 })
 export class UserService {
-  apiurl = 'https://falcon-travel-api.herokuapp.com/';
+  apiurl = 'https://falcon-travel-api.herokuapp.com/api';
+
 
   constructor(private http: HttpClient) {
 
+
    }
+  create(userData){
+    return this.http.post<User>(`${this.apiurl}/user/create`, userData)
+  } 
 }
