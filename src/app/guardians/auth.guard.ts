fba11981f22731b3e7b64c478804eb1ec69019dd
PaolaCuadros1 <paola.cuadroso@gmail.com>
2020-09-03 21:41:17 -0500

    Se creó el guardian y se validaron las rutas.

---
+++ src/app/guardians/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+import { StorageService } from '../services/storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor( 
+    private storageService: StorageService,
+    private route: Router
+   ){}
+
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      if(this.storageService.getToken() != null){
+        return true;
+      }
+      this.route.navigate(['/registerUser'])
+      return false;
+  }
+  
+}
